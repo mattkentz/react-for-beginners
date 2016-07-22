1c012011abbd6809360ddbd79448a00c5f9f50f8
Matthias Kentzia <matthias.kentzia@tipico.com>
2016-07-22 11:02:35 +0200

    Added App, Header, Inventory, Order components

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,73 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 /*
+ * App
+ */
+
+var App = React.createClass({
+  render: function () {
+    return (
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header tagline="Fresh Seafood Market"/>
+        </div>
+        <Order />
+        <Inventory />
+      </div>
+    )
+  }
+});
+
+/*
+ * Header
+ */
+
+var Header = React.createClass({
+  render: function () {
+    return (
+      <header className="top">
+        <h1>Catch
+          <span className="ofThe">
+            <span className="of">
+              of
+            </span>
+            <span className="the">
+              the
+            </span>
+          </span>
+          day
+        </h1>
+        <h3 className="tagline"><span>{this.props.tagline}</span></h3>
+      </header>
+    )
+  }
+});
+
+/*
+ * Inventory
+ */
+
+var Inventory = React.createClass({
+  render: function () {
+    return (
+      <p>Inventory</p>
+    )
+  }
+});
+
+/*
+ * Order
+ */
+
+var Order = React.createClass({
+  render: function () {
+    return (
+      <p>Order</p>
+    )
+  }
+});
+
+/*
  * Store Picker
  * This will let us make <StorePicker />
  */
@@ -22,4 +89,4 @@ var StorePicker = React.createClass({
 });
 
 
-ReactDOM.render(<StorePicker />, document.querySelector('#main'));
+ReactDOM.render(<App />, document.querySelector('#main'));
